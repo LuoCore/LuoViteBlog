c0a4f50d825399c81760bb64d1f0a7123a93ee34
LuoCore <LuoCore@hotmail.com>
2021-12-31 14:06:58 +0800

    添加项目

---
+++ src/components/NoscriptTag.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- Google Tag Manager (noscript) 脚本没有被执行时展示-->
+  <noscript>
+    <iframe
+      src="//www.googletagmanager.com/ns.html?id=GTM-WKV3GT5"
+      height="0"
+      width="0"
+      style="display: none; visibility:visible;"
+    ></iframe>
+  </noscript>
+</template>
